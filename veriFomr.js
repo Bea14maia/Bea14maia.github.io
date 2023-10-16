1fd7936366280e506e477ba39efccde387b95b6b
Bea14maia <139911812+Bea14maia@users.noreply.github.com>
2023-10-16 16:39:09 -0300

    Add files via upload

---
+++ veriFomr.js
@@ -0,0 +1,34 @@
+    function enviar() {
+    // Obtenha os valores dos campos
+    let nome = document.getElementById("nome").value;
+    let data = document.getElementById("data").value;
+    let mae = document.getElementById("mae").value;
+    let pai = document.getElementById("Pai").value;
+    let tele = document.getElementById("tele").value;
+    let tel = document.getElementById("tel").value;
+    let email = document.getElementById("email").value;
+
+    // Verifique se os campos estão preenchidos
+    if (nome === "" || data === "" || mae === "" || pai === "" || tele === "" || tel === "" || email === "") {
+        alert("Por favor, preencha todos os campos obrigatórios.");
+    } else if (email) {
+
+    }
+    
+    
+    else {
+        // Faça alguma ação com os dados, como enviar o formulário
+        alert("Formulário enviado com sucesso!");
+    }
+};
+
+// document.getElementById("limpar").addEventListener("click", function() {
+//     // Limpe os campos do formulário
+//     document.getElementById("nome").value = "";
+//     document.getElementById("data").value = "";
+//     document.getElementById("mae").value = "";
+//     document.getElementById("pai").value = "";
+//     document.getElementById("tele").value = "";
+//     document.getElementById("tel").value = "";
+//     document.getElementById("email").value = "";
+// });
