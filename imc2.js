716b9ddae1bb6218e9c0e2ef11abbaeb6f8b7b43
Bea14maia <139911812+Bea14maia@users.noreply.github.com>
2023-10-17 14:29:57 -0300

    Add files via upload

--- imc2.js
+++ imc2.js
@@ -2,7 +2,7 @@ function calcularIMC() {
     const peso = parseFloat(document.getElementById("peso").value);
     const altura = parseFloat(document.getElementById("altura").value);
     const nome = document.getElementById("name").value;
-    const imc = (peso/(altura*altura).toFixed(1));
+    const imc = (peso/(altura*altura)).toFixed(1);
 
     document.querySelector("p").innerHTML =  `Olá ${nome} , seu IMC é de ${imc}`;
 
